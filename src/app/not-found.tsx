1ce542a507e642e6065efd3c9b8e5b23e48b839f
Michal Jirák <majklmajklson@gmail.com>
2026-08-18 18:29:27 +0200

    404 redirect na homepage v pripade neexistujici routy (fallback pro jiz indexovane stranky ze stareho webu)

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,4 +1,4 @@
-import Link from "next/link";
+import { redirect } from "next/navigation";
 import { notFoundPageMetadata } from "@/lib/pageMetadata";
 
 export const metadata = notFoundPageMetadata;
@@ -9,17 +9,5 @@ export const viewport = {
 };
 
 export default function NotFound() {
-  return (
-    <div className="min-h-screen flex items-center justify-center text-center p-8 -mt-20">
-      <div>
-        <h3 className="text-4xl font-bold mb-4">Stránka nenalezena</h3>
-        <p className="text-lg text-gray-600">
-          Omlouvám se, ale tato stránka neexistuje.
-        </p>
-        <Link href="/" className="mt-6 btn-base btn-primary w-fit mx-auto">
-          Zpět na domovskou stránku
-        </Link>
-      </div>
-    </div>
-  );
+  redirect("/");
 }
